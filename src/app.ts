bed493c5655eb5e7ab21b01faa9b7c3a9d881d3c
Jacob Rose <rose@insevis.de>
2022-04-01 13:44:51 +0200

    reorginized routes

--- src/app.ts
+++ src/app.ts
@@ -1,33 +1,20 @@
 import express, { Express, Request, Response } from 'express';
+import ejs from 'ejs';
 import path from 'path';
+
+import indexRouter from './routes/index';
+import authRouter from './routes/auth';
 
 const app: Express = express();
 
 app.set( "views", path.join( __dirname, "views" ) );
+app.set('view engine', 'ejs');
+
+
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.get('/', (req: Request, res: Response) => {
-  res.sendFile('views/index.html', {root: __dirname });
-});
+app.use('/', indexRouter);
+app.use('/', authRouter);
 
-app.get('/login', (req: Request, res: Response) => {
-  res.sendFile('views/login.html', {root: __dirname });
-});
-
-app.get('/signup', (req: Request, res: Response) => {
-  res.sendFile('views/signup.html', {root: __dirname });
-});
-
-app.post('/login', (req: Request, res: Response) => {
-  console.log('you are logged in!')
-});
-
-app.post('/signup', (req: Request, res: Response) => {
-  console.log('you are signed up!')
-});
-
-app.post('/logout', (req: Request, res: Response) => {
-  console.log('you are logged out!')
-});
 
 export default app;
---
+++ src/routes/auth.ts
@@ -0,0 +1,28 @@
+import express, { Request, Response } from 'express';
+import passport from 'passport'
+import LocalStrategy from 'passport-local'
+import crypto from 'crypto'
+
+const router = express.Router();
+
+router.get('/login', (req: Request, res: Response) => {
+    res.render('login');
+});
+  
+router.get('/signup', (req: Request, res: Response) => {
+    res.render('signup');
+});
+  
+router.post('/login', (req: Request, res: Response) => {
+    console.log('you are logged in!')
+});
+  
+router.post('/signup', (req: Request, res: Response) => {
+    console.log('you are signed up!')
+});
+  
+router.post('/logout', (req: Request, res: Response) => {
+    console.log('you are logged out!')
+});
+
+export default router;
